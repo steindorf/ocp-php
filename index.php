f15afa3e3fa4b93bef45f04584a9d62bdddf6017
Magnus Glantz <open.grieves@gmail.com>
2017-02-06 23:05:42 +0100

    Ver-fix

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
  <body>
  <?php echo 'App version 9.5: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
- <?php echo ' Current PHP version: ' . phpversion(); . phpversion(); ?>
+ <?php echo ' Current PHP version: ' . phpversion(); ?>
  </body>
 </html>
