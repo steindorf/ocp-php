5416e4928ffd350ba333490fd566598d9bb066f8
Magnus Glantz <sudo@redhat.com>
2017-03-31 09:57:58 +0200

    Significant updates

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 10.0. Pod IP: '; ?>
+ <?php echo 'App Version: 10.1. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
