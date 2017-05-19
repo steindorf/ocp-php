d68dfb6d5f7940567435964bd967a4f822aab8e0
Steindór Frímannsson <steindorf@tmsoftware.is>
2017-05-19 10:05:17 +0000

    Small changes
    Blabla

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 10.2. Pod IP: '; ?>
+ <?php echo 'App Version: 10.3. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
